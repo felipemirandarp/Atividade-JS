88fcfd48f5152b611ae582084920672f9189c517
Felipe Miranda <felipemirandaresendepinheiro@gmail.com>
2024-09-17 22:57:43 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let nota1 = prompt('Digite a primeira nota: ') 
+let nota2 = prompt('Digite a segunda nota: ')  
+let nota3 = prompt('Digite a terceira nota: ') 
+ 
+var media = ((nota1 * 1 ) + (nota2 * 2 )+ (nota3 * 3 )) / (1+2+3)
+
+alert('A media e:  ' + media)
